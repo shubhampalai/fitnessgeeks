92328ece0452f069e4c3d360f9b753c604c21b0e
Shubham Palai <112258904+shubhampalai@users.noreply.github.com>
2023-01-22 16:04:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { NavBar } from "./components/NavBar";
+import { Home } from './pages/Home';
+import { ExerciseDetail } from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+import './App.css';
+
+//rafce
+
+const App = () => {
+    return (
+        <Box width="400px" sx={{
+            width: { xl: '1488px' },
+        }} m="auto">
+            <NavBar />
+            <Routes>
+
+                <Route path="/" element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetail />} />
+            </Routes>
+            <Footer />
+        </Box>
+    );
+}
+
+export default App;
---
+++ src/utils/fetchData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+export const exerciseOptions = {
+
+    headers: {
+        'X-RapidAPI-Key': ' 350b42e73cmsh0ceaa1e0f3e81cfp163ad4jsn2a93eeb14e37 ',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+
+export const youtubeOptions = {
+
+    headers: {
+        'X-RapidAPI-Key': 'cc1d09a0bemsh7db7b7f7f5c3bbfp1ef401jsn0d3f0066672a',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+
+}
+
+export const fetchData = async (url, options) => {
+
+    const response = await axios.get(url, options);
+    return response;
+}
+
